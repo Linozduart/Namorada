7959b03272316d8e0789b191f5815d55c581c91d
Indra <42115827+Linozduart@users.noreply.github.com>
2025-06-11 16:42:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarCodigo() {
   const codigo = document.getElementById("codigo").value.trim().toUpperCase();
   const conteudo = document.getElementById("conteudo");
 
-  if (codigo === "A") {
+  if (codigo === "STITCH2025") {
     conteudo.classList.remove("escondido");
   } else {
     alert("Código incorreto! Tente novamente ❤️");
